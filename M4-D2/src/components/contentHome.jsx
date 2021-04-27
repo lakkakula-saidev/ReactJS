acface5950dacacf349262f7df0e54fe2ed92743
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-04-27 17:08:21 +0200

    Cards styled

--- M4-D2/src/components/contentHome.jsx
+++ M4-D2/src/components/contentHome.jsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react'
 import React from 'react'
 import {Jumbotron,Button, Container,Card,Row, Col} from 'react-bootstrap'
 import LatestRelease from '../data/fantasy.json'
+import '../styles/cardstyle.css'
 
 
 class Content extends React.Component{
@@ -11,15 +12,14 @@ render(){
         <Container fluid>
 
         <Row className="justify-content-between mx-2">{LatestRelease.map(book=>{
-            return(<Col><Card style={{ width: '15rem' }} key={book.asin}>
-  <Card.Img variant="top" src={book.img} />
+            return(<Col className='mb-4'><Card style={{ width: '15rem' }} key={book.asin}>
+  <Card.Img className ='cardImages' variant="top" src={book.img} />
   <Card.Body>
-    <Card.Title>{book.title}</Card.Title>
-    <Card.Text>
+    <Card.Title className='titleWrap'>{book.title}</Card.Title>
+    <Card.Text >
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
   </Card.Body>
 </Card></Col>)})}</Row>
             
